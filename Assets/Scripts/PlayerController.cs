1623aebd2384bf3920884f5a38f3ea4540a1c2c6
Christopher Wind <christopherwind843@gmail.com>
2019-09-17 15:37:08 -0400

    adds UI, adds script for counting pickup objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     //check every frame for player input and apply that input to the player game object
     //every frame as movement
 
-   public float speed;
+    public float speed;
+    public Text countText; //holds reference to UI text component 
     private Rigidbody rb; //create a variable of type Rigidbody
+    private int count; //variable to hold the number of pickups weve collected
 
     void Start(){
         rb= GetComponent<Rigidbody>(); //finds and returns a reference to the rigidbody
         //object in the game, if there is one. in our case, the player (ball)
+        count=0;
+        SetCountText(); //initializes countText
     }
     void FixedUpdate(){ //called before performing physics calculations, physics code
     //because our ball moves with physics, this code will use fixedupdate() instead of
@@ -28,7 +33,12 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up")){//checks to see if collider is a "pickup"
             other. gameObject.SetActive(false); // deactivates "pickup" collider, 
             //eliminates its presence in the game scene
+            count= count + 1; 
+            SetCountText();
         }
     
+    }
+    void SetCountText (){
+         countText.text= "Count: "+count.ToString();
     }
 }
