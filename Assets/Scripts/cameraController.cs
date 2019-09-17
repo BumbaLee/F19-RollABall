bd746c5035fd6cb76051ef5ec650ad580f195dde
Christopher Wind <christopherwind843@gmail.com>
2019-09-17 13:04:30 -0400

    cameraController script added

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //at start of game, establish distance between camera and player and apply
+        //to a variable
+        offset= transform.position - player.transform.position;
+    }
+
+    // use LateUpdate() instead of Update() because it requires player to move before
+    //making changes. Otherwise, camera position will keep changing, even if player 
+    //stands still
+    void LateUpdate()
+    {
+        //for every frame, add the offset to the player position to maintain 
+        //steady camera position
+        transform.position=player.transform.position+offset;
+    }
+}
